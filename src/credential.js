84384f3b58ec77566a88ea90e6eafae81279667e
Yuncong <shjdzhangyuncong@sjtu.edu.cn>
2018-09-10 22:27:23 +0800

    try: add template credential.js

---
+++ src/credential.js
@@ -0,0 +1,27 @@
+module.exports = {
+	encrypted: false,
+	data: JSON.stringify({
+    keys: {
+      zb: {
+        access_key: '',
+        secret_key: ''
+      },
+      okex: {
+        access_key: '',
+        secret_key: ''
+      },
+      binance: {
+        access_key: '',
+        secret_key: ''
+      }
+    },
+		username:       'username',
+		password:       'password',
+		email_username: '123456789@qq.com',
+		email_password: 'qqpassword',
+		tomail:         'blabla@foo.com',
+		mail_host:      'smtp.qq.com',
+		mail_port:      465,
+	})
+}
+
