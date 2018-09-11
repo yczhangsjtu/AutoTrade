f77c25b04c4b7f402b3d23b767a83b909159e24a
Yuncong <shjdzhangyuncong@sjtu.edu.cn>
2018-09-11 09:53:35 +0800

    try: implement login

--- client/src/Login.js
+++ client/src/Login.js
@@ -5,19 +5,19 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return (
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <div className="row">
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
           <form action="/api/login" method="post">
-            <div class="form-group">
-              <label class="form-label" for="username">Username</label>
-              <input type="text" class="form-control" name="username" id="username" aria-describedby="User Name" placeholder="Username" />
+            <div className="form-group">
+              <label className="form-label" htmlFor="username">Username</label>
+              <input type="text" className="form-control" name="username" id="username" aria-describedby="User Name" placeholder="Username" />
             </div>
-            <div class="form-group">
-              <label class="form-label" for="password">Password</label>
-              <input type="password" class="form-control" name="password" id="password" placeholder="Password" />
+            <div className="form-group">
+              <label className="form-label" htmlFor="password">Password</label>
+              <input type="password" className="form-control" name="password" id="password" placeholder="Password" />
             </div>
-            <div class="form-check">
-              <button type="submit" class="btn btn-primary">Submit</button>
+            <div className="form-check">
+              <button type="submit" className="btn btn-primary">Submit</button>
             </div>
 
           </form>
